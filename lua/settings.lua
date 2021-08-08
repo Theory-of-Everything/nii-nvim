8aa73a6b47cbc5a054e59c490b5a598e4e2fdf57
Theory_of_Everything <evan_sarris@protonmail.ch>
2021-08-08 09:48:14 -0600

    Added WIP easy settings changing script settings.lua

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,7 +1,12 @@
 local cmd = vim.cmd
+local opt = vim.opt
+local g = vim.g
 local o = vim.o
 
-cmd('syntax enable')
-cmd('set nu')
-o.completeopt = "menuone,noselect"
--- cmd('set splitbelow splitright!')
+local settings = {}
+
+function enable_mosue(case)
+	cmd('set mouse='..case)
+end
+
+return settings
