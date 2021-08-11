1a1a97db20dc6a14c8754bc08d8b3bc898a194ea
Theory_of_Everything <evan_sarris@protonmail.ch>
2021-08-11 00:10:04 -0600

    Forgot to add lualine theme support for night-owl

---
+++ lua/themes/lualine/night-owl.lua
@@ -0,0 +1,48 @@
+-- Copyright (c) 2021 Jnhtr
+-- MIT license, see LICENSE for more details.
+-- LuaFormatter off
+local colors = {
+  base_fg 	= '#011627',
+  light_fg	= '#D6DEEB',
+  secondary	= '#575656',
+  background	= '#23384A',
+  norm_bg 	= '#ADDB67',
+  ins_bg	= '#EF5350',
+  vis_bg	= '#C792EA',
+  rep_bg	= '#82AAFF',
+  cmd_bg	= '#21C7A8',
+  int_bg	= '#282C34',
+}
+--LuaFormatter on
+return {
+  normal = {
+    a = {bg = colors.norm_bg, fg = colors.base_fg, gui = 'bold'},
+    b = {bg = colors.secondary, fg = colors.light_fg},
+    c = {bg = colors.background, fg = colors.light_fg},
+  },
+  insert = {
+    a = {bg = colors.ins_bg, fg = colors.base_fg, gui = 'bold'},
+    b = {bg = colors.secondary, fg = colors.light_fg},
+    c = {bg = colors.background, fg = colors.light_fg}
+  },
+  visual = {
+    a = {bg = colors.vis_bg, fg = colors.base_fg, gui = 'bold'},
+    b = {bg = colors.secondary, fg = colors.light_fg},
+    c = {bg = colors.background, fg = colors.light_fg}
+  },
+  replace = {
+    a = {bg = colors.rep_bg, fg = colors.base_fg, gui = 'bold'},
+    b = {bg = colors.secondary, fg = colors.light_fg},
+    c = {bg = colors.background, fg = colors.light_fg}
+  },
+  command = {
+    a = {bg = colors.cmd_bg, fg = colors.base_fg, gui = 'bold'},
+    b = {bg = colors.secondary, fg = colors.light_fg},
+    c = {bg = colors.background, fg = colors.light_fg}
+  },
+  inactive = {
+    a = {bg = colors.int_bg, fg = colors.light_fg, gui = 'bold'},
+    b = {bg = colors.secondary, fg = colors.light_fg},
+    c = {bg = colors.background, fg = colors.light_fg}
+  }
+}
