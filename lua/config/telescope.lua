7f4511573d578c49026cf6cf785440ff9b2a6eb0
Theory_of_Everything <evan_sarris@protonmail.ch>
2021-09-05 01:03:49 -0600

    Cleaned up the formatting for most of the larger config files

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,11 +1,11 @@
-require('telescope').setup {
+require("telescope").setup({
 	extensions = {
 		fzf = {
 			fuzzy = true,
 			override_genearic_sorter = false,
 			override_file_sorter = true,
 			case_mode = "smart_case",
-		}
-	}
-}
-require('telescope').load_extension('fzf')
+		},
+	},
+})
+require("telescope").load_extension("fzf")
