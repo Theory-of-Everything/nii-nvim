66986f3bd59bf5416b0fa65e80691fe33982b097
Theory_of_Everything <evan_sarris@protonmail.ch>
2021-09-15 19:52:37 -0600

    Updated scheme lib similar to the previous commit

--- lua/lib/scheme.lua
+++ lua/lib/scheme.lua
@@ -1,7 +1,22 @@
+
+--[[
+scheme.lua
+This file is wrapper code for handiling loading themes for the statusline
+and for neovim
+
+Lualine has some default themes that are bundled with it, so lualine_def_themes
+contains a list of all the default themes for lualine
+
+Themes are primarily loaded via require()ing files in the lua/themes/
+directory
+
+--]]
+
+-- module definition
 local scheme = {}
 
+-- list of pre-packaged lualine themes
 local lualine_def_themes = {
-	-- list of installed themes from the lualine repo
 	"16color",
 	"ayu_dark",
 	"ayu_light",
@@ -34,12 +49,16 @@ local lualine_def_themes = {
 	"wombat",
 }
 
+-- if the scheme bundled with lualine?
 scheme.is_lualine_default = false
 
+-- pretty wrapper for loading theme files
 function scheme.load_scheme(choice)
 	require("themes." .. choice)
 end
 
+-- checks if the arg is a theme in the default themes list,
+-- otherwise it requires a file
 function scheme.load_lualine_scheme(choice)
 	scheme.scheme = choice
 	local is_present = false
@@ -56,9 +75,11 @@ function scheme.load_lualine_scheme(choice)
 	end
 end
 
+-- loads both editor and statusline scheme
+-- simultaniously
 function scheme.load_shared_scheme(choice)
-	require("themes." .. choice)
-	scheme.scheme = choice
+   require("themes." .. choice)
+   scheme.load_lualine_scheme(choice)
 end
 
 return scheme
