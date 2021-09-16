40ac695cdb6e72f9f1e6fa41af9ab8d3d23e5e2c
Theory_of_Everything <evan_sarris@protonmail.ch>
2021-09-15 19:49:53 -0600

    Reworked scheme lib
    This rework focuses on implementing the default lualine themes into the
    scheme library, and not manually creating theme files for themes. This
    reduces work to integrate with themes and the amount of files in the
    lualine themes dir.

--- lua/config/plug/lualine.lua
+++ lua/config/plug/lualine.lua
@@ -1,13 +1,16 @@
 local scheme = require("lib.scheme")
 
+-- current set lualine theme
 local lualine_theme = nil
 
+-- if a scheme is not bundled with lualine, look for a theme file
 if scheme.is_lualine_default == false then
 	lualine_theme = require("themes.lualine." .. scheme.scheme)
 else
     lualine_theme = scheme.scheme
 end
 
+-- lualine setup config
 require("lualine").setup({
 	options = {
 		section_separators = { "", "" },
