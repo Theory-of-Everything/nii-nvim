7f47789619a417464865df1fff416b6a2063f246
TheoryToE <evan@theoryware.net>
2022-02-08 21:31:20 -0700

    fix: lspkind/treesitter breakage
    closes ticket #1[1] (issue #27[2] on gh)
    
    [1]: https://todo.sr.ht/~theorytoe/nii-nvim-bugs/1
    [2]: https://github.com/Theory-of-Everything/nii-nvim/issues/27

--- lua/config/plug/treesitter.lua
+++ lua/config/plug/treesitter.lua
@@ -1,11 +1,20 @@
 require('nvim-treesitter.configs').setup({
-	ensure_installed = {
-		-- "maintained",
-	},
-	ignore_install = {}, -- List of parsers to ignore installing
+	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = 'maintained',
+
+	-- Install languages synchronously (only applied to `ensure_installed`)
+	sync_install = false,
+
+	-- List of parsers to ignore installing
+	ignore_install = {},
+
 	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = {}, -- list of language that will be disabled
+		-- `false` will disable the whole extension
+		enable = true,
+
+		-- list of language that will be disabled
+		disable = {},
+
 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
