592d2e1332fee6c16cefee093e7a2edb4b9ff3f3
vishal <chocotacove@gmail.com>
2022-05-04 00:42:10 +0530

    update: treesitter and nvimtree config

--- lua/config/plug/treesitter.lua
+++ lua/config/plug/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup({
-	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
-	ensure_installed = 'maintained',
+	-- One of "all" or a list of languages
+	ensure_installed = {},
 
 	-- Install languages synchronously (only applied to `ensure_installed`)
 	sync_install = false,
